f2d6d58a590fb33ca70bd89d302ceb10e69e172c
Diki Haryadi <diki.haryadi@mncgroup.com>
2024-09-09 14:29:28 +0700

    feat: add isPalindrome number

---
+++ easy/palindrome_number/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	isPalindrome(121)
+	isPalindrome(5432112345)
+	isPalindrome(-121)
+	isPalindrome(10)
+	isPalindrome(123321)
+	isPalindrome(1234321)
+	isPalindrome(12344321)
+
+}
---
+++ easy/palindrome_number/palindrom_number.go
@@ -0,0 +1,13 @@
+package main
+
+func isPalindrome(x int) bool {
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+	reversedNumber := 0
+	for x > reversedNumber {
+		reversedNumber = reversedNumber*10 + x%10
+		x /= 10
+	}
+	return x == reversedNumber || x == reversedNumber/10
+}
